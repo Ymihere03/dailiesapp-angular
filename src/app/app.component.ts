cdcbaf779b226b7cd5697ac832363f0caab990ce
Ben Darling <ymihere03@gmail.com>
2025-04-01 10:37:28 -0600

    begin ssr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DOCUMENT, isPlatformBrowser, isPlatformServer } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'dailiesapp-angular';
+  private readonly platform = inject(PLATFORM_ID);
+  private readonly document = inject(DOCUMENT);
+
+  constructor() {
+      if (isPlatformBrowser(this.platform)) {
+          console.warn("browser");
+          // Safe to use document, window, localStorage, etc. :-)
+          //console.log(document);
+      }
+
+      if (isPlatformServer(this.platform)) {
+          console.warn("server");
+          // Not smart to use document here, however, we can inject it ;-)
+          //console.log(this.document);
+      }
+  }
 }
